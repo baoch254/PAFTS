58a337c5351ffeea4f66c16f0e7c4084148151ee
Bao Cao <baoch254@gmail.com>
2024-11-06 16:48:23 +0700

    add token

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
 from pafts import PAFTS
+
 
 p = PAFTS(
     path="/kaggle/input/testaudio/",
     output_path="/kaggle/working",
+    hf_token=str(os.getenv("HF_TOKEN")),
 )
 
 # Separator
